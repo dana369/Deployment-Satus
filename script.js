b9084d2489e09b4e3f58f3898fbd949fea03363f
dana369 <dananjana@onramper.com>
2023-05-23 23:04:31 +0530

    update

---
+++ script.js
@@ -0,0 +1,31 @@
+window.addEventListener("DOMContentLoaded", function() {
+    fetch('data.json')
+      .then(response => response.json())
+      .then(data => {
+        const tableBody = document.getElementById("myTableBody");
+        data.forEach(item => {
+          const row = document.createElement("tr");
+          const serviceCell = document.createElement("td");
+          const stgddCell = document.createElement("td");
+          const stgdlCell = document.createElement("td");
+          const prodddCell = document.createElement("td");
+          const proddlCell = document.createElement("td");
+  
+          serviceCell.textContent = item.service;
+          stgddCell.textContent = item.stgdd;
+          stgdlCell.textContent = item.stgdl;
+          prodddCell.textContent = item.proddd;
+          proddlCell.textContent = item.proddl;
+
+          row.appendChild(serviceCell);
+          row.appendChild(stgddCell);
+          row.appendChild(stgdlCell);
+          row.appendChild(prodddCell);
+          row.appendChild(proddlCell);
+  
+          tableBody.appendChild(row);
+        });
+      })
+      .catch(error => console.error('Error:', error));
+  });
+  
