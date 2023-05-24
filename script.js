46f592753f7e75ca62da8cc91fedb8c8ed1803b1
dana369 <dananjana@onramper.com>
2023-05-24 16:55:37 +0530

    update

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-window.addEventListener("DOMContentLoaded", function() {
-    fetch('data.json')
-      .then(response => response.json())
-      .then(data => {
-        const tableBody = document.getElementById("myTableBody");
-        data.forEach(item => {
-          const row = document.createElement("tr");
-          const serviceCell = document.createElement("td");
-          const stgddCell = document.createElement("td");
-          const stgdlCell = document.createElement("td");
-          const prodddCell = document.createElement("td");
-          const proddlCell = document.createElement("td");
+// Function to fetch Lambda function details
+function getLambdaFunctionDetails() {
+    AWS.config.update({ region: 'us-east-1' }); // Replace with your desired AWS region
+    var lambda = new AWS.Lambda();
+    var params = {
+      FunctionName: 'quotes-stg' // Replace with your Lambda function name
+    };
   
-          serviceCell.textContent = item.service;
-          stgddCell.textContent = item.stgdd;
-          stgdlCell.textContent = item.stgdl;
-          prodddCell.textContent = item.proddd;
-          proddlCell.textContent = item.proddl;
-
-          row.appendChild(serviceCell);
-          row.appendChild(stgddCell);
-          row.appendChild(stgdlCell);
-          row.appendChild(prodddCell);
-          row.appendChild(proddlCell);
+    lambda.getFunction(params, function (err, data) {
+      if (err) {
+        console.log('Error:', err);
+        document.getElementById('function-details').innerHTML = '<p>An error occurred while fetching Lambda function details.</p>';
+      } else {
+        var description = data.Configuration.Description;
+        var lastModified = data.Configuration.LastModified;
   
-          tableBody.appendChild(row);
-        });
-      })
-      .catch(error => console.error('Error:', error));
-  });
+        // Update the website content with the Lambda function details
+        document.getElementById('description').innerHTML = description;
+        document.getElementById('last-modified').innerHTML = lastModified;
+      }
+    });
+  }
+  
+  // Call the function when the page loads
+  window.onload = getLambdaFunctionDetails;
   
